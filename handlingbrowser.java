ee8d72733d0081971bbafc4b293c9a47d11ade20
dhanush1-m <129542066+dhanush1-m@users.noreply.github.com>
2023-04-03 07:35:19 +0530

    Update handlingbrowser.java

--- handlingbrowser.java
+++ handlingbrowser.java
@@ -11,10 +11,10 @@ public class handlingbrowser {
 			System.setProperty("webdriver.chrome.driver", "./chrome/chromedriver.exe");
 			WebDriver driver=new ChromeDriver();
 			Thread.sleep(2000);
-		     driver.get("https://demoqa.com/browser-windows");
-		     Thread.sleep(2000);
-			    driver.findElement(By.id("windowButton")).click();
-		    String p = driver.getWindowHandle();
+		   driver.get("https://demoqa.com/browser-windows");
+		   Thread.sleep(2000);
+		   driver.findElement(By.id("windowButton")).click();
+		   String p = driver.getWindowHandle();
 		   Set<String> pc = driver.getWindowHandles();
 		   int o = pc.size();  
 		   System.out.println(o);
@@ -22,13 +22,12 @@ public class handlingbrowser {
 		   int f = pc.size();
 		   System.out.println(f);
 		   
-	for(String b:pc){
-	driver.switchTo().window(b);
-	Thread.sleep(2000);
-	driver.close();
+	          for(String b:pc){
+	          driver.switchTo().window(b);
+	          Thread.sleep(2000);
+	          driver.close();
 		
 	}
-		}
-
+	}
 	}
 
